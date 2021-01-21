8eff132fcbdf6a7feba64706127023fbbf48bb93
Kelvin <kelvin.silva@sinqia.com.br>
2021-01-21 11:05:10 -0300

    test(task1/Address): Adiciona testes base para o service Address

---
+++ task1/luizalabs/src/test/java/com/kelvincarvalho/luizalabs/service/AddressServiceImplTest.java
@@ -0,0 +1,27 @@
+package com.kelvincarvalho.luizalabs.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+public class AddressServiceImplTest {
+
+
+    @Test
+    public void shouldThrowExceptionWhenPostalCodeIsInvalid() {
+        // TODO
+    }
+
+    @Test
+    public void shouldAddZeroInThePostalCodeWhenNotFoundAddress() {
+        // TODO
+    }
+
+    @Test
+    public void shouldReturnAddressWhenPostalCodeIsValid() {
+        // TODO
+    }
+
+
+}
