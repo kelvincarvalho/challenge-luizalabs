62cc04262a2c589d0509ec4a0fabadec208224e2
Kelvin Carvalho da Silva <kelvinc_silva@hotmail.com>
2021-01-21 11:09:46 -0300

    feature(task1/Address): Adiciona model Address

---
+++ task1/luizalabs/src/main/java/com/kelvincarvalho/luizalabs/model/AddressDTO.java
@@ -0,0 +1,19 @@
+package com.kelvincarvalho.luizalabs.model;
+
+import java.io.Serializable;
+
+public class AddressDTO implements Serializable {
+
+    private static final long serialVersionUID = -7215784916671002962L;
+
+    private String streetAddress;
+
+    private String city;
+
+    private String state;
+
+    private String country;
+
+    private int postalCode;
+
+}
