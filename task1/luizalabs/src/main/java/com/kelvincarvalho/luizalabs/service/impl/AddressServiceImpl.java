2421a2145b9f1fdce29acaa7fd8e37c040cca2ad
Kelvin Carvalho da Silva <kelvinc_silva@hotmail.com>
2021-01-21 16:38:39 -0300

    feature(task1/Address): Adiciona validação inicial

--- task1/luizalabs/src/main/java/com/kelvincarvalho/luizalabs/service/impl/AddressServiceImpl.java
+++ task1/luizalabs/src/main/java/com/kelvincarvalho/luizalabs/service/impl/AddressServiceImpl.java
@@ -2,15 +2,26 @@ package com.kelvincarvalho.luizalabs.service.impl;
 
 import com.kelvincarvalho.luizalabs.model.AddressDTO;
 import com.kelvincarvalho.luizalabs.service.AddressService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AddressServiceImpl implements AddressService {
+public class AddressServiceImpl implements AddressService  {
 
     @Override
     public AddressDTO getAddress(String zipCode) {
+        var zipCodeValid = this.validateZipCode(zipCode);
+
         return null;
+    }
+
+    private String validateZipCode(String zipCode) {
+        zipCode = zipCode.replace("-", "");
+
+        if(zipCode.length() != 8 ) {
+            // TODO: add error
+        }
+
+        return zipCode;
     }
 
 }
