c5d920f321c60cfe84e58fad923fc33d01bd3e5d
Kelvin Carvalho da Silva <kelvinc_silva@hotmail.com>
2021-01-25 19:49:05 -0300

    refactor(task1/dto): Refatora DTO Address

--- task1/luizalabs/src/main/java/com/kelvincarvalho/luizalabs/model/AddressDTO.java
+++ task1/luizalabs/src/main/java/com/kelvincarvalho/luizalabs/model/AddressDTO.java
@@ -1,27 +1,58 @@
 package com.kelvincarvalho.luizalabs.model;
 
+import com.fasterxml.jackson.annotation.JsonGetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
-@Data
 @Builder
 @AllArgsConstructor
 public class AddressDTO implements Serializable {
 
-    private static final long serialVersionUID = -7215784916671002962L;
+    private static final long serialVersionUID = -8863384049589074718L;
 
     private String streetAddress;
+
+    private String neighborhood;
 
     private String city;
 
     private String state;
 
-    private String country;
-
     private String zipCode;
+
+    @JsonGetter(value = "streetAddress")
+    public String getStreetAddress() { return streetAddress; }
+
+    @JsonSetter(value = "logradouro")
+    public void setStreetAddress(String streetAddress) { this.streetAddress = streetAddress; }
+
+    @JsonGetter(value = "neighborhood")
+    public String getNeighborhood() { return neighborhood; }
+
+    @JsonSetter(value = "bairro")
+    public void setNeighborhood(String neighborhood) { this.neighborhood = neighborhood; }
+
+    @JsonGetter(value = "city")
+    public String getCity() { return city; }
+
+    @JsonSetter(value = "localidade")
+    public void setCity(String city) { this.city = city; }
+
+    @JsonGetter(value = "state")
+    public String getState() { return state; }
+
+    @JsonSetter(value = "uf")
+    public void setState(String state) { this.state = state; }
+
+    @JsonGetter(value = "zipCode")
+    public String getZipCode() { return zipCode; }
+
+    @JsonSetter(value = "cep")
+    public void setZipCode(String zipCode) { this.zipCode = zipCode; }
+
 
 }
